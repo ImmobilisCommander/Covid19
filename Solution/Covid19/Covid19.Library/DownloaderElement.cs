5bdb03b56c7bf561974184620898890960cf3490
Julien <julien_lefevre@outlook.fr>
2020-05-02 17:45:40 +0200

    One file class

---
+++ Solution/Covid19/Covid19.Library/DownloaderElement.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+
+namespace Covid19.Library
+{
+    public class DownloaderElement : ConfigurationElement
+    {
+        /// <summary>
+        /// Whether download should be forced eventhough file already exists in repository
+        /// </summary>
+        [ConfigurationProperty("forceDownload")]
+        public bool? ForceDownload { get { return (bool?)base["forceDownload"]; } }
+
+        /// <summary>
+        /// Identity name
+        /// </summary>
+        [ConfigurationProperty("name")]
+        public string Name { get { return (string)base["name"]; } }
+
+        /// <summary>
+        /// Identity email
+        /// </summary>
+        [ConfigurationProperty("email")]
+        public string Email { get { return (string)base["email"]; } }
+
+        /// <summary>
+        /// Root path to the repository
+        /// </summary>
+        [ConfigurationProperty("repositoryPath")]
+        public string RepositoryPath { get { return (string)base["repositoryPath"]; } }
+    }
+}
--- Solution/Covid19/Covid19.Library/ProviderElement.cs
+++ Solution/Covid19/Covid19.Library/ProviderElement.cs
@@ -25,31 +25,4 @@ namespace Covid19.Library
         [ConfigurationProperty("outputFile")]
         public string OutputFile { get { return (string)base["outputFile"]; } }
     }
-
-    public class DownloaderElement : ConfigurationElement
-    {
-        /// <summary>
-        /// Whether download should be forced eventhough file already exists in repository
-        /// </summary>
-        [ConfigurationProperty("forceDownload")]
-        public bool? ForceDownload { get { return (bool?)base["forceDownload"]; } }
-
-        /// <summary>
-        /// Identity name
-        /// </summary>
-        [ConfigurationProperty("name")]
-        public string Name { get { return (string)base["name"]; } }
-
-        /// <summary>
-        /// Identity email
-        /// </summary>
-        [ConfigurationProperty("email")]
-        public string Email { get { return (string)base["email"]; } }
-
-        /// <summary>
-        /// Root path to the repository
-        /// </summary>
-        [ConfigurationProperty("repositoryPath")]
-        public string RepositoryPath { get { return (string)base["repositoryPath"]; } }
-    }
 }
